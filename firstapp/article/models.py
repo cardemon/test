cd83e047206546ce47e1f3652aea20586a7f12fc
Cardemon <ruslan555@ukr.net>
2016-03-30 21:54:15 +0300

    lol

---
+++ firstapp/article/models.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Article(models.Model):
+	"""docstring for Article"""
+	class Meta(object):
+		db_table = "article"
+	article_title = models.CharField(max_length = 200)
+	article_text = models.TextField()
+	article_date = models.DateTimeField()
+	article_likes = models.IntegerField(default = 0)
+
+class Comments(models.Model):
+	class Meta(object):
+		db_table = 'comments'
+
+	comments_text = models.TextField()
+	comments_article = models.ForeignKey(Article)
---
+++ firstapp/article/urls.py
@@ -0,0 +1,24 @@
+"""firstapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, patterns, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^1/', 'article.views.basic_one'),
+    url(r'^2/', 'article.views.template_two'),
+    url(r'^3/', 'article.views.template_three_simple'),
+
+]
